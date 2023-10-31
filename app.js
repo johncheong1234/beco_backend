4c688c00de135bba89ff46d813120d9118503404
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-10-31 20:59:55 +0800

    roman numeral conversion

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 import express from 'express';
 import bodyParser from "body-parser";
 import cors from "cors";
-import { ValidationOfRomanNumerals } from './utils.js';
+import {
+    ValidationOfRomanNumerals,
+    romanToDecimal
+} from './utils.js';
 
 const app = express();
-app.use(cors()) 
+app.use(cors())
 
 var jsonParser = bodyParser.json()
 
-app.post('/convert',jsonParser, async (req, res) => {
+app.post('/convert', jsonParser, async (req, res) => {
     let { word } = req.body;
 
-    if (ValidationOfRomanNumerals(word)){
-        res.send('true')
-    }else{
-        res.send('false')
+    if (ValidationOfRomanNumerals(word)) {
+        let romanNumeral = romanToDecimal(word);
+        res.send(`${romanNumeral}`)
+    } else {
+        res.send('not a roman numeral')
     }
 })
- 
+
 
 // Server listen
 app.listen(3001, () => console.log("Server listening to port 3001"));
--- utils.js
+++ utils.js
@@ -2,13 +2,13 @@ export const ValidationOfRomanNumerals = (str) => {
     // Regex to check valid
     // ROMAN NUMERAL
     let regex = new RegExp(/^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/);
- 
+
     // str
     // is empty return false
     if (str == null) {
         return false;
     }
- 
+
     // Return true if the str
     // matched the ReGex
     if (regex.test(str) == true) {
@@ -17,4 +17,58 @@ export const ValidationOfRomanNumerals = (str) => {
     else {
         return false;
     }
+}
+
+function value(r) {
+    if (r == 'I')
+        return 1;
+    if (r == 'V')
+        return 5;
+    if (r == 'X')
+        return 10;
+    if (r == 'L')
+        return 50;
+    if (r == 'C')
+        return 100;
+    if (r == 'D')
+        return 500;
+    if (r == 'M')
+        return 1000;
+    return -1;
+}
+
+export const romanToDecimal = (str) => {
+
+    // Initialize result
+    var res = 0;
+
+    for (let i = 0; i < str.length; i++) {
+
+        // Getting value of symbol s[i]
+        var s1 = value(str.charAt(i));
+
+        // Getting value of symbol s[i+1]
+        if (i + 1 < str.length) {
+            var s2 = value(str.charAt(i + 1));
+
+            // Comparing both values
+            if (s1 >= s2) {
+                // Value of current symbol
+                // is greater or equalto
+                // the next symbol
+                res = res + s1;
+            }
+            else {
+
+                // Value of current symbol is
+                // less than the next symbol
+                res = res + s2 - s1;
+                i++;
+            }
+        } else {
+            res = res + s1;
+        }
+    }
+
+    return res;
 }
